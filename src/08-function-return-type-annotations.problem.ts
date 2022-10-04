d4886a866401f369d2b1ed76ed182d59debee46e
Jarda Korinek <korinek14.j@gmail.com>
2022-10-04 21:17:13 +0200

    exercise 08 completed

--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -17,8 +17,19 @@ interface Post {
  * How do we ensure that makeUser ALWAYS
  * returns a user?
  */
-const makeUser = () => {
-  return {};
+const makeUser = (): User => {
+  return {
+    id: 1,
+    firstName: "Jarda",
+    lastName: "Kořínek",
+    role: "super-admin",
+    posts: [
+      {
+        id: 1,
+        title: "I am god",
+      },
+    ],
+  };
 };
 
 it("Should return a valid user", () => {

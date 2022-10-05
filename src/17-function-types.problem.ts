a47214a2e80afb1e822126aef9da8374533c9274
Jarda Korinek <korinek14.j@gmail.com>
2022-10-05 23:23:05 +0200

    exercise 17 completed -> Typing Functions

--- src/17-function-types.problem.ts
+++ src/17-function-types.problem.ts
@@ -3,7 +3,9 @@ import { Equal, Expect } from "./helpers/type-utils";
 /**
  * How do we type onFocusChange?
  */
-const addListener = (onFocusChange: unknown) => {
+type FocusListener = (isFocused: boolean) => void;
+
+const addListener = (onFocusChange: FocusListener) => {
   window.addEventListener("focus", () => {
     onFocusChange(true);
   });
@@ -12,6 +14,36 @@ const addListener = (onFocusChange: unknown) => {
     onFocusChange(false);
   });
 };
+
+// const addListener = (onFocusChange: (isFocused: boolean) => void) => {
+//   window.addEventListener("focus", () => {
+//     onFocusChange(true);
+//   });
+
+//   window.addEventListener("blur", () => {
+//     onFocusChange(false);
+//   });
+// };
+
+// const addListener = (onFocusChange: (arg0: boolean) => void) => {
+//   window.addEventListener("focus", () => {
+//     onFocusChange(true);
+//   });
+
+//   window.addEventListener("blur", () => {
+//     onFocusChange(false);
+//   });
+// };
+
+// const addListener = (fn: (onFocusChange: boolean) => void) => {
+//   window.addEventListener("focus", () => {
+//     fn(true);
+//   });
+
+//   window.addEventListener("blur", () => {
+//     fn(false);
+//   });
+// };
 
 addListener((isFocused) => {
   console.log({ isFocused });
